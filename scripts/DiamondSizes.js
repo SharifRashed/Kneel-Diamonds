d22b876738d121444afa847459d4e10a02062ed8
sharifrashed <sharif.rashed26@gmail.com>
2021-12-01 10:24:49 -0600

    first commit: imports are done

--- scripts/DiamondSizes.js
+++ scripts/DiamondSizes.js
@@ -15,12 +15,14 @@ export const DiamondSizes = () => {
     let html = "<ul>"
 
     // Use .map() for converting objects to <li> elements
+    // interates the array, just like for...of does BUT invokes the function that you define
     const listItems = sizes.map(size => {
         return `<li>
             <input type="radio" name="size" value="${size.id}" /> ${size.carets}
         </li>`
     })
 
+    // .join() -- joins all of the individual items in the array into one single string
     html += listItems.join("")
     html += "</ul>"
 
